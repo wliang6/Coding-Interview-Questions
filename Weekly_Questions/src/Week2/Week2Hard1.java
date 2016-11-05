34784d61112243a5ad86afe777c8977f01d21aca
Winnie Liang <wliang6@buffalo.edu>
2016-11-04 23:41:10 -0400

    Week 2 Hard questions complete

--- Weekly_Questions/src/Week2/Week2Hard1.java
+++ Weekly_Questions/src/Week2/Week2Hard1.java
@@ -1,10 +1,78 @@
 package Week2;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 /**
  * Weekly_Questions
  * @author Winnie Nov 2, 2016
  * Week2Hard1.java
+ * Question: Given a word, return the shortest possible palindrome that 
+ * can built by adding characters to the front or end of the word.
+ * What is a palindrome? A palindrome is a word that is identical to itself 
+ * when reversed. For example, “otto” is a palindrome, and so is “oto”. “Cat” is not a palindrome. 
+ * What is the input to the function? The function takes a string that may or may not be a palindrome.
+ * What is the output to the function? The function returns a string that is 
+ * a palindrome and containing the given word. Characters must be added to either the 
+ * front or back of the word to accomplish this.
+ * What are some good test cases? 
+ * ["hi" -> “ihi”, "o" -> “o”, "hii" -> “hiih”, "iih" -> “hiih”, "haahfr" -> “rfhaahfr”, "frhaah” -> “frhaah”, "haifriend" -> “dneirfiahaifriend”]
+ * If characters could be added to either end of the word and it would
+ * make two different palindromes of the same length, which should I return? Great question, it doesn’t  matter as long as you pick one and be consistent with it.
+ * What if I’m given an empty string? Great question. Return an empty string.
+
  */
 public class Week2Hard1 {
 
+	@Test public void test1(){ assertEquals("ihi", palindrome("hi")); } 
+	@Test public void test2(){ assertEquals("o", palindrome("o")); }
+	@Test public void test3(){ assertEquals("hiih", palindrome("hii")); }
+	@Test public void test4(){ assertEquals("hiih", palindrome("iih")); }
+	@Test public void test5(){ assertEquals("rfhaahfr", palindrome("haahfr")); }
+	@Test public void test6(){ assertEquals("frhaahrf", palindrome("frhaah")); }
+	@Test public void test7(){ assertEquals("dneirfiahaifriend", palindrome("haifriend")); }
+	@Test public void test8(){ assertEquals("pofafclcfafop", palindrome("pofafcl")); }
+	
+	public static void main(String [] args){
+		String[] testcases = new String[] {"otto", "", "hi", "o", "hii", "iih", "haahfr", "frhaah", "haifriend", "pofafcl"};
+		for(String s : testcases){
+			System.out.println(s + " : " + palindrome(s));
+		}
+	}
+	
+	
+	public static String palindrome(String s){
+		String palin = "";
+		if(s.length() == 0 || s.length() == 1) return s;
+		if(checkPalin(s)) return s;
+		for(int i = 0; i < s.length(); i++){
+			if(checkPalin(s.substring(0, i))){
+				palin = reverse(s.substring(i)) + s;
+			}
+			else if(checkPalin(s.substring(i))){
+				palin = s + reverse(s.substring(0, i));
+			}
+		}	
+		return palin;
+	}
+	
+	
+	public static boolean checkPalin(String s){
+		for(int i = 0; i < s.length()/2; i++){
+			if(s.charAt(i) != s.charAt(s.length() - 1 - i)){
+				return false;
+			}	
+		}
+		return true;
+	}
+	
+	public static String reverse(String s){
+		if(s.length() == 0 || s.length() == 1) return s;
+		String reversed = "";
+		for(int i = s.length() - 1; i >= 0; i--){
+			reversed += s.charAt(i);
+		}
+		return reversed;
+	}
 }
--- Weekly_Questions/src/Week2/Week2Hard2.java
+++ Weekly_Questions/src/Week2/Week2Hard2.java
@@ -1,10 +1,42 @@
 package Week2;
 
+import java.util.ArrayList;
 /**
  * Weekly_Questions
  * @author Winnie Nov 2, 2016
  * Week2Hard2.java
+ * Question: The problem is #7 from Project Euler: By listing the 
+ * first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
+ * the 6th prime is 13. What is the 10001st prime number?
+ * Can I use a library function to check if a number is prime? 
+ * You should be able to do this relatively efficiently 
+ * (at least for the test case of the 10001st prime number) 
+ * without using a library function.
  */
 public class Week2Hard2 {
 
+	public static void main(String[] args){
+		System.out.println(nthPrimeNumber(10001));
+	}
+
+	public static int nthPrimeNumber(int num){
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(2);
+		list.add(3);
+		int counter = 4;
+		while(list.size() < num){
+			if(counter%2 != 0 && counter%3 != 0){   //if number is not multiple of 2 nor 3
+				int temp = 4;
+				while(temp*temp <= counter){   
+					if(counter%temp == 0) break;
+					temp++;
+				}
+				if(temp*temp > counter){   //checks if temp^2 is greater than the counter
+					list.add(counter);
+				}
+			}
+			counter++;
+		}
+		return list.get(num-1);
+	}
 }
